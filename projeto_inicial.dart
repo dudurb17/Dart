6c385e578fb0c991ade39556be583c5328394ba9
dudurb17 <eduardo.robettibedin@gmail.com>
2024-05-03 18:49:08 -0300

    var, final, const

--- projeto_inicial.dart
+++ projeto_inicial.dart
@@ -1,7 +1,10 @@
 void main() {
-  int idade = 19;
+  var idade = 19;
+  final verdadeiro = true;
+  const String nome = "Eduardo";
+
+  
   double num = 1.43;
-  bool verdadeiro = true;
   bool falso = false;
   String teste = "teste";
   print(teste);
@@ -9,6 +12,6 @@ void main() {
 
   List<String> listanomes = ['Ricarth', "fernando", "eduardo", "ana"];
   print(listanomes);
-  List<dynamic> eduardo=['Eduardo Robetii Bedin', 1.80, 19];
+  List<dynamic> eduardo = [nome, 1.80, idade];
   print(eduardo);
 }
