9edc2e1bff0f95aa4872552c2e6119f83c34e0ef
dudurb17 <eduardo.robettibedin@gmail.com>
2024-05-03 18:25:26 -0300

    lista em dart

--- projeto_inicial.dart
+++ projeto_inicial.dart
@@ -1,7 +1,12 @@
 void main() {
   int idade = 19;
-  double num=1.43;
-  bool verdadeiro=true;
-  bool falso=false;
-  print(idade + num);
+  double num = 1.43;
+  bool verdadeiro = true;
+  bool falso = false;
+  String teste = "teste";
+  print(teste);
+  print("tenho $idade anos");
+
+  List<String> listanomes = ['Ricarth', "fernando", "eduardo", "ana"];
+  print(listanomes);
 }
