b847d53946f0a5d9a6ce04d2e67169300f956511
dudurb17 <eduardo.robettibedin@gmail.com>
2024-05-07 08:00:26 -0300

    utilizando o switch

--- dart_application_2/bin/dart_application_2.dart
+++ dart_application_2/bin/dart_application_2.dart
@@ -14,12 +14,38 @@ void escolherMeioTransporte(int locomocao) {
 }
 
 void escolherMeioTransporteEnum(Transporte locomocao) {
-  if (locomocao == Transporte.carro) {
-    print("Vou de CARRo para aventura");
-  } else if (locomocao == Transporte.bike) {
-    print("Vou de BIKE para aventura");
-  } else {
-    print("Vou para aventura");
+  // if (locomocao == Transporte.carro) {
+  //   print("Vou de CARRo para aventura");
+  // } else if (locomocao == Transporte.bike) {
+  //   print("Vou de BIKE para aventura");
+  // } else {
+  //   print("Vou para aventura");
+  // }
+
+  switch (locomocao) {
+    case Transporte.carro:
+      print("Vou de carro para aventura");
+      break;
+    case Transporte.bike:
+      print("Vou de carro para aventura");
+      break;
+    case Transporte.andando:
+      print("Vou de Trem para aventura");
+      break;
+    case Transporte.skate:
+      print("Vou de skate para aventura");
+      break;
+    case Transporte.aviao:
+      print("Vou de aviao para aventura");
+      break;
+    case Transporte.patins:
+      print("Vou de patins para aventura");
+      break;
+    case Transporte.trem:
+      print("Vou de trem para aventura");
+      break;
+    default:
+      print("Nao achei a categoria q vc vai passear");
   }
 }
 
